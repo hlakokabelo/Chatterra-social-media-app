719c689fd3a54398201b8cb06ed4407657629067
Kabelo Hlako <kabelohlako.kh@gmail.com>
2026-03-05 01:25:56 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -29,9 +29,9 @@ const NavBar: React.FunctionComponent<INavBarProps> = () => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="flex font-mono text-xl font-bold text-white">
-           <img 
-          className="w-[25px] mr-2" src={image} alt="" />
-          Chatterra</Link>
+            <img className="w-[25px] mr-2" src={image} alt="" />
+            Chatterra
+          </Link>
 
           {/**Desktop links */}
           <div className="hidden md:flex items-center space-x-8">
