ca7bd8892719deb556123bb126e8a93ae45e2fef
Kabelo Hlako <55398494+hlakokabelo@users.noreply.github.com>
2026-05-13 15:51:01 +0200

    Added Vercel insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 const client = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
       <AuthProvider>
         <BrowserRouter>
           <App />
+          <SpeedInsights />
         </BrowserRouter>
       </AuthProvider>
     </QueryClientProvider>
