9b6bbdfc67f1c2b8d211179c79fd825597de0dc6
Kabelo Hlako <55398494+hlakokabelo@users.noreply.github.com>
2026-05-13 15:56:17 +0200

    Vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const client = new QueryClient();
@@ -17,9 +18,10 @@ createRoot(document.getElementById("root")!).render(
       <AuthProvider>
         <BrowserRouter>
           <App />
+          <Analytics />
           <SpeedInsights />
         </BrowserRouter>
       </AuthProvider>
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 );
